28f75d677c0b9715646b602edab25219fc70db8c
chad <cvbm41@hotmail.com>
2025-11-27 14:14:16 +0900

    251127

---
+++ js/global.js
@@ -0,0 +1,9 @@
+
+if (location.hostname === "namu-dw.com") {
+  apiGlobalURL = "https://250204.aisystem64.org";
+  console.log("Using Production API URL");
+} else if (location.hostname === "rr720.synology.me") {
+//   apiGlobalURL = "http://127.0.0.1:8000";
+  apiGlobalURL = "https://250204.aisystem64.org";
+  console.log("Using Local API URL");
+}
